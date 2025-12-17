73eb0aec1cbaac56a7de89948ed8c7e055479b95
Wilson Saadeh <wilson_saadeh@hotmail.com>
2025-12-16 22:00:11 -0300

    seeding Posts

--- src/main/java/com/devsaadeh/post/config/TestConfig.java
+++ src/main/java/com/devsaadeh/post/config/TestConfig.java
@@ -1,5 +1,8 @@
 package com.devsaadeh.post.config;
 
+import com.devsaadeh.post.models.embedded.Author;
+import com.devsaadeh.post.models.embedded.Comment;
+import com.devsaadeh.post.models.entities.Post;
 import com.devsaadeh.post.models.entities.User;
 import com.devsaadeh.post.repositories.PostRepository;
 import com.devsaadeh.post.repositories.UserRepository;
@@ -8,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import java.time.Instant;
 import java.util.Arrays;
 
 @Configuration
@@ -38,9 +42,24 @@ public class TestConfig {
 
         User maria = new User(null,"Maria Brown","maria@gmail.com");
         User alex = new User(null,"Alex Green","alex@gmail.com");
-        User bob = new User(null,"Bob Gray","bob@gmail.com");
+        User bob = new User(null,"Bob Grey","bob@gmail.com");
 
         repository.saveAll(Arrays.asList(maria,alex,bob));
+
+        Post post1 = new Post(null, Instant.parse("2025-12-13T11:15:01Z"), "Partiu viagem", "Vou viajar para São Paulo. Abraços!", new Author(maria));
+        Post post2 = new Post(null, Instant.parse("2025-12-14T10:05:49Z"), "Bom dia", "Acordei feliz hoje!", new Author(maria));
+
+        Comment c1 = new Comment("Boa viagem mano!", Instant.parse("2025-12-13T14:30:01Z"), new Author(alex));
+        Comment c2 = new Comment("Aproveite", Instant.parse("2025-12-13T15:38:05Z"), new Author(bob));
+        Comment c3 = new Comment("Tenha um ótimo dia!", Instant.parse("2025-12-14T12:34:26Z"), new Author(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().addAll(Arrays.asList(c3));
+
+        postRepository.saveAll(Arrays.asList(post1, post2));
+
+        maria.getPosts().addAll(Arrays.asList(post1, post2));
+        repository.save(maria);
 
     }
 }
--- src/main/java/com/devsaadeh/post/models/entities/User.java
+++ src/main/java/com/devsaadeh/post/models/entities/User.java
@@ -49,4 +49,8 @@ public class User {
     public void setEmail(String email) {
         this.email = email;
     }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
 }
